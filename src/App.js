01c2a547b2b4918cdf11649c66711865ebb0a545
Ruchikon005 <6135512005@psu.ac.th>
2020-09-11 10:14:10 +0700

    Check2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const word = "Hello";
 function App() {
   return (
     <div>
-      Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      Array.from(word).map((c, i) => <Character value={c} key={i}/>)
     </div>
   );
 }
